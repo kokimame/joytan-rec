79ddbb766a001b97ec9ba450a41acb529479977d
kokimame <kohkimametani@gmail.com>
2019-09-09 13:27:11 +0900

    Done basic sign up backend code

--- app/src/main/java/com/joytan/rec/setting/SignupActivity.kt
+++ app/src/main/java/com/joytan/rec/setting/SignupActivity.kt
@@ -3,14 +3,20 @@ package com.joytan.rec.setting
 //import android.support.v7.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 import com.joytan.rec.R
 import com.joytan.rec.analytics.AnalyticsHandler
 
 import kotlinx.android.synthetic.main.activity_setting.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 /**
  * 設定画面
@@ -18,13 +24,14 @@ import kotlinx.android.synthetic.main.activity_setting.*
 class SignupActivity : AppCompatActivity() {
 
     private val ah = AnalyticsHandler()
+    private val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         //ツールバーの設定
         setSupportActionBar(toolbar)
-//        setTheme(R.style.SettingsFragmentStyle)
+        setTheme(R.style.AuthStyle)
 
         val lsab = supportActionBar
         lsab?.setHomeButtonEnabled(true)
@@ -33,12 +40,82 @@ class SignupActivity : AppCompatActivity() {
         //Analytics
         ah.onCreate(this)
 
-        val loginBtn = findViewById<Button>(R.id.btn_to_login)
-        loginBtn.setOnClickListener{
+        val toLoginBtn = findViewById<Button>(R.id.btn_to_login)
+        toLoginBtn.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
+        val doSignupBtn = findViewById<Button>(R.id.btn_do_signup)
+        doSignupBtn.setOnClickListener{
+            trySigningUp()
+        }
+    }
+
+    fun trySigningUp() {
+        Log.i("kohki", "Do registering")
+        Log.i("kohki", mAuth.currentUser.toString())
+
+        val emailText = findViewById<EditText>(R.id.signup_email).text.toString()
+        val pswText = findViewById<EditText>(R.id.signup_psw).text.toString()
+        val confirmPswText = findViewById<EditText>(R.id.confirm_password).text.toString()
+        val unameText = findViewById<EditText>(R.id.signup_uname).text.toString()
+
+        if (!validateTextInput(emailText, pswText, confirmPswText)) {
+            return
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(emailText, pswText)
+                .addOnCompleteListener(this, OnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = mAuth.currentUser
+                        val uid = user!!.uid
+                        Toast.makeText(this, "Successfully registered :)",
+                                Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "Error registering, try again later :(",
+                                Toast.LENGTH_LONG).show()
+                    }
+                })
+    }
+
+    fun validateTextInput(emailText: String, pswText: String, confirmPswText: String) : Boolean{
+        if (emailText.isEmpty()) {
+            Toast.makeText(this, "Please enter your email address.", Toast.LENGTH_LONG).show()
+            return false
+        } else if (pswText.isEmpty()) {
+            Toast.makeText(this, "Please enter your password.", Toast.LENGTH_LONG).show()
+            return false
+        } else if (confirmPswText.isEmpty()) {
+            Toast.makeText(this, "Please confirm your password.", Toast.LENGTH_LONG).show()
+            return false
+        } else if (confirmPswText.isEmpty()) {
+            Toast.makeText(this, "Please confirm your password.", Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (pswText.length < 8) {
+            Toast.makeText(this, "Please make your password longer than 8 characters.",
+                    Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            Toast.makeText(this, "Please enter an valid email address.", Toast.LENGTH_LONG).show()
+            return false
+        }
+
+
+        if (pswText != confirmPswText) {
+            Toast.makeText(this, "Confirmation password does not match.", Toast.LENGTH_LONG).show()
+            return false
+        }
+        return true
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("kohki", mAuth.currentUser.toString())
     }
 
     override fun onResume() {
